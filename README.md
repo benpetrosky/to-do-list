f79dd70fbb1b1b26901efef01a3b4413f98defef
benpetrosky@hotmail.com and Chitroopa Manikkavasagam <benpetrosky@hotmail.com@>
2017-04-03 15:15:45 -0700

    add experimental click function to todo list

---
+++ README.md
@@ -0,0 +1,44 @@
+# _To-do List_
+
+#### _Creates a to-do list, 04/03/2017_
+
+#### By _**Chitroopa Manikkavasagam, Ben Petrosky**_
+
+## Description
+
+_This is a web application that takes input from a user about tasks they want to accomplish and puts them into a todo list. Upon clicking on todo list item the task is moved into an "accomplished" list._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open the repository and double click on index.html file_
+
+## Known Bugs
+
+_Nil_
+
+## Specifications
+
+* _Doesn't accept blank input._
+    * _**Example Input** :  _ (omission)_
+    * _**Example Output** : Please fill all information_
+
+* _It concatenates task and deadline before adding to the to-do list._
+    * _**Example Input** : task: dishes, deadline: 04/30/2017_
+    * _**Example Output** : dishes by 04/30/2017_
+
+* _It concatenates a checkmark to task and "is DONE!" string to the accomplished list when the user clicks on todo-item._
+    * _**Example todo list item** : dishes by 04/30/2017_
+    * _**Example Output** : (checkmark sym) dishes is DONE!_
+
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, JavaScript and jQuery_
+
+
+### License
+
+Copyright (c) 2017 **_Chitroopa Manikkavasagam, Ben Petrosky_**
+
+*This software is licensed under the MIT licence*
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-function Todo(task, date, deadline)
+function Todo(task, deadline)
 {
   this.taskname = task;
-  this.todayDate = date;
   this.deadlineDate = deadline;
 }
 Todo.prototype.todoString = function(){
   return this.taskname + " by " + this.deadlineDate;
 }
 Todo.prototype.accomplishedString = function(){
-  return this.taskname + " IS DONE!";
+  return  "&#10003" + " " + this.taskname + " IS DONE!";
 }
 
 $(function(){
@@ -16,26 +15,24 @@ $(function(){
     event.preventDefault();
 
     var taskInput = $("#task").val();
-    var todayDateInput = $("#today-date").val();
     var deadlineInput = $("#deadline").val();
 
-    if(!taskInput || !todayDateInput || !deadlineInput)
+    if(!taskInput || !deadlineInput)
     {
       alert("Please fill all information");
     }
     else
     {
-      var newTask = new Todo(taskInput, todayDateInput, deadlineInput);
+      var newTask = new Todo(taskInput, deadlineInput);
 
       $("#result-to-do-list").prepend("<li><span class='todo-item'>" + newTask.todoString() + "</span></li>");
 
       $("#task").val("");
-      $("#today-date").val("");
       $("#deadline").val("");
 
       $(".todo-item").click(function() {
         this.remove();
-        $("#accomplished").prepend("<li><span class='accomplished-item'>" + newTask.taskname + "</span></li>")
+        $("#accomplished").append("<li><span class='accomplished-item'>" + newTask.accomplishedString() + "</span></li>")
       });
     }
   });
